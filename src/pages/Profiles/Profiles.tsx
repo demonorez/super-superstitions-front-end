b4e72247a0dd8a45644d1b08fb5dfe71de01d301
Mike Robles <mikej.robles@gmail.com>
2023-02-26 13:56:01 -0500

    refactor profiles page

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,26 +1,13 @@
-// npm packages
-import { useState, useEffect } from 'react'
-
-// services
-import * as profileService from '../../services/profileService'
-
 // types
 import { Profile } from '../../types/models'
 
-const Profiles = (): JSX.Element => {
-  const [profiles, setProfiles] = useState<Profile[]>([])
+interface ProfileProps {
+  profiles: Profile[]
+}
 
-  useEffect((): void => {
-    const fetchProfiles = async (): Promise<void> => {
-      try {
-        const profileData: Profile[] = await profileService.getAllProfiles()
-        setProfiles(profileData)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchProfiles()
-  }, [])
+const Profiles = (props: ProfileProps): JSX.Element => {
+  
+  const {profiles} = props
 
   if(!profiles.length) return <p>No profiles yet</p>
 
@@ -33,5 +20,5 @@ const Profiles = (): JSX.Element => {
     </>
   )
 }
- 
+
 export default Profiles
