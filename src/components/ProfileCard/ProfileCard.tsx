618021a596d5de1fe138eda09c9b15a8b713f725
Mike Robles <mikej.robles@gmail.com>
2023-02-28 18:55:29 -0500

    removes link on ProfileCard component

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,8 +1,6 @@
 //types
 import { Profile } from '../../types/models'
 
-import { Link } from 'react-router-dom'
-
 interface ProfileCardProps {
   profile: Profile
 }
@@ -13,12 +11,10 @@ const ProfileCard = (props: ProfileCardProps): JSX.Element => {
   const profilePic = profile.photo
 
   return (
-    <Link to={`/profiles/${profile.id}`}>
       <article>
         <img src={profilePic} alt={`${profile.name}'s avatar`} />
         <h1>{profile.name}</h1>
       </article>
-    </Link>
   )
 }
 
